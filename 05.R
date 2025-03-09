6833915ae6fbefab6f24da902efe723e799aa521
Mitch Macdonald <35157616+mcmacdonald@users.noreply.github.com>
2025-03-09 23:27:37 +0000

    update

--- 05.R
+++ 05.R
@@ -14,7 +14,8 @@ clustering <- function(g){
   require("statnet"); require("igraph"); require("intergraph")
    cc <- igraph::transitivity(
      intergraph::asIgraph(g),
-     type = "globalundirected"
+     type = "localaverageundirected",
+     isolates = "zero"
      )
   print(cc)
 }
@@ -31,63 +32,75 @@ cc_super <- clustering(g_super)
 
 
 
-# function to generate the clustering coefficients of of random graphs the have similar degree distribution 
-sampler <- function(g){
+
+
+# function to generate the clustering coefficients of simulated graphs from the latent space model
+simulator <- function(g){
   
   # required packages
-  require('statnet'); require('igraph'); require('intergraph')
+  require('statnet'); require('igraph'); require('intergraph'); require('latentnet'); require('statip')
   
   # set seed for replication purposes
-  set.seed(20190812) # Hayes' birthday
+  seed <- set.seed(20190812) # Hayes' birthday
   
-  # dimensions of the graph
-  n <- length(igraph::V(intergraph::asIgraph(g))) # 'n' nodes
-  m <- length(igraph::E(intergraph::asIgraph(g))) # 'm' edges
+  # calculate the modal shortest path length
+  d <- igraph::distances(intergraph::asIgraph(g))
+  d <- as.vector(d) 
+  d <- statip::mfv(d) # mode
   
-  # function to generate random graphs from the Erdos-Renyi model
-  samples <- 10000 # number of random graphs to generate
-  results <- c() # results vector
+  # latent space model, controlling for the squared euclidean distance, where the dimension set to the modal shortest path
+  model <- latentnet::ergmm(g ~ euclidean2(d = 2), seed = seed)
+  
+  # simulate large number of networks from the latent space model
+  simulations <- simulate(model, nsim = 10000, seed = seed)
+  simulations <- simulations$networks   # retain the simulated networks
+  
+  # number of simulations
+  samples <- length(simulations)
+  
+  # store results in list
+  results <- c()
+  
+  # loop for the simulations
   for(i in 1:samples){
-    # random graphs
-    g_random = igraph::sample_gnm(
-      n = n,
-      m = m, # m edges taken from the uniform random distribution from the set of all possible edges 
-      directed = FALSE, 
-      loops = FALSE
-      )
     
-    # calculate clustering coefficient for the random graphs
+    # calculate the clustering coefficient for each of the simulations
     cc <- igraph::transitivity(
-      g_random,
-      type = "globalundirected"
-      )
+      graph = intergraph::asIgraph(simulations[[i]]),
+      type = "localaverageundirected",
+      isolates = "zero"
+       )
     
     # store the clustering coefficients in the results vector
     results <- c(results, cc)
   }
   results <- as.data.frame(results)
   colnames(results) <- c("cc")
+  
+  # return
   return(results)
 }
-cc_siren.random <- sampler(g_siren)
-cc_togo.random <- sampler(g_togo)
-cc_caviar.random <- sampler(g_caviar)
-cc_cielnet.random <- sampler(g_cielnet)
-cc_cocaine.random <- sampler(g_cocaine)
-cc_heroin.random <- sampler(g_heroin)
-cc_oversize.random <- sampler(g_oversize)
-cc_montagna.random <- sampler(g_montagna)
-cc_tfc.random <- sampler(g_tfc)
-cc_super.random <- sampler(g_super)
+cc_siren.lsm <- simulator(g_siren)
+cc_togo.lsm <- simulator(g_togo)
+cc_caviar.lsm <- simulator(g_caviar)
+cc_cielnet.lsm <- simulator(g_cielnet)
+cc_cocaine.lsm <- simulator(g_cocaine)
+cc_heroin.lsm <- simulator(g_heroin)
+cc_oversize.lsm <- simulator(g_oversize)
+cc_montagna.lsm <- simulator(g_montagna)
+cc_tfc.lsm <- simulator(g_tfc)
+# cc_super.lsm <- simulator(g_super)
 
 
 
-# plot histogram of the clustering coefficients for the real and random graphs
-plot_clustering <- function(coeff, random, title){
+
+
+# plot histogram of the clustering coefficients for the real and simulated graphs
+plot_clustering <- function(coeff, lsm, title){
   require('ggplot2'); require('scales'); library('ggplot2')
-  label1 <- mean(random$cc); label1 <- round(label1, digits = 2) # label to annotate the mean coefficient of the random networks
+  label1 <- mean(lsm$cc); label1 <- round(label1, digits = 2) # label to annotate the mean coefficient of the simulated networks
   label2 <- round(coeff, digits = 2) # label to annotate the coefficient for the criminal networks
-  histogram <- ggplot2::ggplot(random, ggplot2::aes( x = cc ) ) + 
+  histogram <- ggplot2::ggplot(lsm, ggplot2::aes( x = cc ) ) + 
     # ggplot2::geom_histogram(ggplot2::aes(y = stat(density)), bins = 25, color = "black", fill = "white") +
     ggplot2::geom_histogram(bins = 100, color = "black", fill = "white") +
     # line marker for the mean clustering coefficient for the random networks
@@ -101,15 +114,15 @@ plot_clustering <- function(coeff, random, title){
     ggplot2::scale_y_continuous(
       name = "PROBABILITY DENSITY FUNCTION (PDF)", 
       labels = scales::percent_format(accuracy = 1L), # 2L to round to one decimal place, 3L to round to two decimal places, etc.
-      limits = c(0.00, 0.29), 
-      breaks = c(0.00, 0.05, 0.10, 0.15, 0.20, 0.25)
-    ) +
+      limits = c(0.00, 0.19), 
+      breaks = c(0.00, 0.05, 0.10, 0.15)
+      ) +
     ggplot2::scale_x_continuous(
       name = "CLUSTERING COEFFICIENT",
       labels = scales::label_number(accuracy = 0.01),
-      limits = c(0.00, 0.50),
-      breaks = c(0.00, 0.10, 0.20, 0.30, 0.40, 0.50)
-    ) +
+      limits = c(0.00, 1.00),
+      breaks = c(0.00, 0.10, 0.20, 0.30, 0.40, 0.50, 0.60, 0.70, 0.80, 0.90, 1.00)
+      ) +
     ggplot2::ggtitle(title) +
     ggthemes::theme_clean() +
     ggplot2::theme(
@@ -123,53 +136,48 @@ plot_clustering <- function(coeff, random, title){
 }
 clustering_siren <- plot_clustering(
   coeff = cc_siren,
-  random = cc_siren.random, 
+  lsm = cc_siren.lsm, 
   title = "(A) SIREN AUTO THEFT RING"
   )
 clustering_togo <- plot_clustering(
   coeff = cc_togo, 
-  random = cc_togo.random, 
+  lsm = cc_togo.lsm, 
   title = "(B) TOGO AUTO THEFT RING"
   )
 clustering_caviar <- plot_clustering(
   coeff = cc_caviar, 
-  random = cc_caviar.random, 
+  lsm = cc_caviar.lsm, 
   title = "(C) CAVIAR DRUG TRAFFICKING ORGANIZATION"
   )
 clustering_cielnet <- plot_clustering(
   coeff = cc_cielnet, 
-  random = cc_cielnet.random, 
+  lsm = cc_cielnet.lsm, 
   title = "(D) CIELNET DRUG TRAFFICKING ORGANIZATION"
   )
 clustering_cocaine <- plot_clustering(
   coeff = cc_cocaine, 
-  random = cc_cocaine.random, 
+  lsm = cc_cocaine.lsm, 
   title = "(E) LA COSA NOSTRA COCAINE TRAFFICKING OUTFIT"
   )
 clustering_heroin <- plot_clustering(
   coeff = cc_heroin, 
-  random = cc_heroin.random, 
+  lsm = cc_heroin.lsm, 
   title = "(F) NEW YORK CITY HEROIN TRAFFICKERS"
   )
 clustering_oversize <- plot_clustering(
   coeff = cc_oversize, 
-  random = cc_oversize.random, 
+  lsm = cc_oversize.lsm, 
   title = "(G) 'NDRANGHETA WIRETAPS - OPERATION OVERSIZE"
   )
 clustering_montagna <- plot_clustering(
   coeff = cc_montagna, 
-  random = cc_montagna.random, 
+  lsm = cc_montagna.lsm, 
   title = "(H) COSA NOSTRA WIRETAPS - OPERATION MONTAGNA"
   )
 clustering_tfc <- plot_clustering(
   coeff = cc_tfc, 
-  random = cc_tfc.random, 
+  lsm = cc_tfc.lsm, 
   title = "(I) THE FRENCH CONNECTION - FEDERAL BUREAU OF NARCOTICS"
-  )
-clustering_super <- plot_clustering(
-  coeff = cc_super, 
-  random = cc_super.random, 
-  title = "(J) SUPER POPULATION OF CRIMINAL NETWORKS"
   )
 
 
@@ -195,7 +203,7 @@ output(plot = clustering_heroin, filename = "fig4f.pdf")
 output(plot = clustering_oversize, filename = "fig4g.pdf")
 output(plot = clustering_montagna, filename = "fig4h.pdf")
 output(plot = clustering_tfc, file = "fig4i.pdf")
-output(plot = clustering_super, filename = "fig4j.pdf")
+
 
 
 
@@ -203,3 +211,77 @@ output(plot = clustering_super, filename = "fig4j.pdf")
 
 
 
+
+
+# function to generate the clustering coefficients of of random graphs the have similar degree distribution 
+sampler <- function(g){
+  
+  # required packages
+  require('statnet'); require('igraph'); require('intergraph')
+  
+  # set seed for replication purposes
+  set.seed(20190812) # Hayes' birthday
+  
+  # dimensions of the graph
+  n <- length(igraph::V(intergraph::asIgraph(g))) # 'n' nodes
+  m <- length(igraph::E(intergraph::asIgraph(g))) # 'm' edges
+  
+  # calculate the modal number of neighbors from the degree distribution
+  # d <- igraph::degree(intergraph::asIgraph(g), mode = "total", loops = FALSE)
+  # d <- mode(d)
+  
+  # calculate the number of neighbors from the degree distribution
+  d <- igraph::degree(intergraph::asIgraph(g), mode = "total", loops = FALSE)
+  
+  # function to generate random graphs from the Erdos-Renyi model
+  samples <- 10000 # number of random graphs to generate
+  results <- c() # results vector
+  for(i in 1:samples){
+    
+    # don't run
+    # generate random graphs
+    # g_random = igraph::sample_gnm(
+    # n = n,
+    # m = m, # m edges taken from the uniform random distribution from the set of all possible edges 
+    # directed = FALSE, 
+    # loops = FALSE
+    # )
+    
+    # randomly select the number of neighbors from the degree distribution
+    nei <- sample(d, 1)
+    
+    # randomly select the probability to rewire edges
+    p <- runif(1, 0, 1)
+    
+    # small world sampler 
+    g_random <- igraph::sample_smallworld(
+      dim = 1, # = 1 for a lattice
+      size = n,
+      nei = nei,
+      p = p
+      )
+    
+    # calculate clustering coefficient for the random graphs
+    cc <- igraph::transitivity(
+      g_random,
+      type = "localaverageundirected",
+      isolates = "zero"
+      )
+    
+    # store the clustering coefficients in the results vector
+    results <- c(results, cc)
+  }
+  results <- as.data.frame(results)
+  colnames(results) <- c("cc")
+  return(results)
+}
+cc_siren.random <- sampler(g_siren)
+cc_togo.random <- sampler(g_togo)
+cc_caviar.random <- sampler(g_caviar)
+cc_cielnet.random <- sampler(g_cielnet)
+cc_cocaine.random <- sampler(g_cocaine)
+cc_heroin.random <- sampler(g_heroin)
+cc_oversize.random <- sampler(g_oversize)
+cc_montagna.random <- sampler(g_montagna)
+cc_tfc.random <- sampler(g_tfc)
+cc_super.random <- sampler(g_super)
